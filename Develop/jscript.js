61587815f16159b3d24a9ce9fce04ec23c7ab55b
guillenjoshua <guillenjoshua@gmail.com>
2020-08-16 10:30:33 -0500

    UVIndex

--- Develop/jscript.js
+++ Develop/jscript.js
@@ -1,46 +1,75 @@
 
 
 
-
-
-
 $("#enterCity").on("click", function() {
-    return getWeather();
- 
-    });
+  return getWeather();
+
+  });
 
 
+// Run Ajax with API Key
 
 function getWeather(){
 
-  var city = $("#city").val();
+var city = $("#city").val();
+var queryCity = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=6b4758acdbe25a3b3e8b792b8978ee96"
+var queryIVIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=6b4758acdbe25a3b3e8b792b8978ee96" + "&lat={lat}" + "&lon={lon}"
+var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=6b4758acdbe25a3b3e8b792b8978ee96"
 
-  if (city != ''){
+if (city != ''){
 
-      $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=6b4758acdbe25a3b3e8b792b8978ee96",
-        type: "GET",
-        dataType: "jsonp",
-        success: function(data){
-            var application = results(data) 
+    $.ajax({
+      url: queryCity, queryForecast,
+      type: "GET",
+      dataType: "jsonp",
+      success: function(data){
+          var application = results(data) 
 
-          $("#displayWeather").html(application);
-          $("#city").val('');
+        $("#displayWeather").html(application);
+        $("#city").val('');
 
-        }
 
-      })
+          var forecast = "";
 
-  }else{
-    $("#error").html("<div>Text Cannot Be Empty</div>"); 
-  }
+          for (var i=0; i<data.list; i++){
+
+            
+          }
+
+
+
+
+      }
+
+    })
 
 }
 
-function results(data){
-    return "<p>" + data.name + "</p>" + 
-             "<p>Temperature: " + data.main.temp + "&deg;F</p>" +
-              "<p>Humidity: " + data.main.humidity + "%</p>" +
-              "<p>Wind Speed: " + data.wind.speed + " MPH</p>"
+}
+
+
+// Cuurent Weather from OpenWeather
+
+var currentCityDiv = $("#currentCity");
+
+const results = function results(data){
+  return "<h2>" + data.name + ", " + data.sys.country + "</h2>" + 
+            "<p>Weather: " + data.weather[0].description + "<img src = http://openweathermap.org/img/wn/" + data.weather[0].icon+ ".png>"  + "</p>" +
+            "<p>Temperature: " + data.main.temp + "&deg;F</p>" +
+            "<p>Humidity: " + data.main.humidity + "%</p>" +
+            "<p>Wind Speed: " + data.wind.speed + " MPH</p>"
+
+    
+
+         
+}
+
+
+//Keep Display
+for (var i = 0; i < results; i++) {
+  
+  if (localStorage.getItem(i)) {
+      $("#" + i).val(localStorage.getItem(i))
+  }
 
 }
