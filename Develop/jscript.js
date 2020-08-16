9c7f55dfd416043402bfbb8a85f54fce6bff9fe9
guillenjoshua <guillenjoshua@gmail.com>
2020-08-16 12:46:34 -0500

    Add Moment js

--- Develop/jscript.js
+++ Develop/jscript.js
@@ -14,12 +14,12 @@ function getWeather(){
 var city = $("#city").val();
 var queryCity = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=6b4758acdbe25a3b3e8b792b8978ee96"
 var queryIVIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=6b4758acdbe25a3b3e8b792b8978ee96" + "&lat={lat}" + "&lon={lon}"
-var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=6b4758acdbe25a3b3e8b792b8978ee96"
+
 
 if (city != ''){
 
     $.ajax({
-      url: queryCity, queryForecast,
+      url: queryCity, 
       type: "GET",
       dataType: "jsonp",
       success: function(data){
@@ -52,14 +52,14 @@ if (city != ''){
 
 var currentCityDiv = $("#currentCity");
 
+var currentDate = moment().format("MMM Do YY");
+
 const results = function results(data){
-  return "<h2>" + data.name + ", " + data.sys.country + "</h2>" + 
+  return "<h2>" + data.name + ", " + data.sys.country + " - " + currentDate + "</h2>" + 
             "<p>Weather: " + data.weather[0].description + "<img src = http://openweathermap.org/img/wn/" + data.weather[0].icon+ ".png>"  + "</p>" +
             "<p>Temperature: " + data.main.temp + "&deg;F</p>" +
             "<p>Humidity: " + data.main.humidity + "%</p>" +
             "<p>Wind Speed: " + data.wind.speed + " MPH</p>"
-
-    
 
          
 }
