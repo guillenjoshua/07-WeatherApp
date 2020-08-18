0992dd3cecb75924e5d14992bbe7e69034925881
guillenjoshua <guillenjoshua@gmail.com>
2020-08-17 21:23:21 -0500

    UVIndex Added-Changed Ajax

--- Develop/jscript.js
+++ Develop/jscript.js
@@ -1,11 +1,18 @@
 
 
 
-$("#enterCity").on("click", function() {
+$("#enterCity").on("click keydown", function() {
      
   return getWeather();
 
 });
+
+
+
+var lat;
+var lon;
+
+
 
 
 // Run API for Current Weather
@@ -16,96 +23,61 @@ $(".container").show();
 
 var city = $("#city").val();
 
+
+
 var queryCity = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=6b4758acdbe25a3b3e8b792b8978ee96"
 
-if (city != ''){
+
+
 
   $.ajax({
     url: queryCity, 
-    type: "GET",
-    dataType: "jsonp",
-    success: function(data){
-        var application = results(data) 
+    method: "GET",
+  }).then(function(city){
+        var application = cityResults(city) 
 
       $("#displayWeather").html(application);
-      $("#city").val('');
+      
+      var queryUVIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=6b4758acdbe25a3b3e8b792b8978ee96&lat="+ city.coord.lat + "&lon=" + city.coord.lon
 
+      $.ajax({
+              url: queryUVIndex, 
+              type: "GET",
+      }).then(function(index){
+                  var ultraViolet = uvResult(index) 
+        
+                $("#Index").html(ultraViolet);
+                
+        
+              });
+        
+
+            });
+
+
+            var currentDate = moment().format("MMM Do YY");
+
+            function cityResults(city){
+              const lat = city.coord.lat
+      
+              const lon = city.coord.lon
+        
+              
+              return "<h2>" + city.name + ", " + city.sys.country + " - " + currentDate + "</h2>" + 
+                        "<p>Weather: " + city.weather[0].description + "<img src = http://openweathermap.org/img/wn/" + city.weather[0].icon+ ".png>"  + "</p>" +
+                        "<p>Temperature: " + city.main.temp + "&deg;F</p>" +
+                        "<p>Humidity: " + city.main.humidity + "%</p>" +
+                        "<p>Wind Speed: " + city.wind.speed + " MPH</p>"
+                                       
+                              }
+
+            function uvResult(index){
+              return "<p> UVIndex: " + index.value + "</p>"
+                    }
+ 
     }
 
-  })
-
-}
-
-}
-
-
-
-// Current Weather from OpenWeather
-
-var currentDate = moment().format("MMM Do YY");
-
-function results(data){
-
-const lat = data.coord.lat
-const lon = data.coord.lon
-
-uvLat = lat
-uvLon = lon
-
-return "<h2>" + data.name + ", " + data.sys.country + " - " + currentDate + "</h2>" + 
-          "<p>Weather: " + data.weather[0].description + "<img src = http://openweathermap.org/img/wn/" + data.weather[0].icon+ ".png>"  + "</p>" +
-          "<p>Temperature: " + data.main.temp + "&deg;F</p>" +
-          "<p>Humidity: " + data.main.humidity + "%</p>" +
-          "<p>Wind Speed: " + data.wind.speed + " MPH</p>" +
-          "<p>lat: " + data.coord.lat + " MPH</p>"
-
-          
-      
-}
-
-
-
-// // Run API for UV Index
-
-// $("#enterCity").on("click", function() {
-//   return getUVIndex();
-
-//   });
-
-
-// function getUVIndex(){
-
-// var city = $("#city").val();
-
-// var queryUVIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=6b4758acdbe25a3b3e8b792b8978ee96&lat=37.75&lon=-122.37"
-
-// if (city != ''){
-
-//     $.ajax({
-//       url: queryUVIndex, 
-//       type: "GET",
-//       dataType: "jsonp",
-//       success: function(data){
-//           var application = results(data) 
-
-//         $("#displayWeather").html(application);
-//         $("#city").val('');
-
-//       }
-
-//     })
-
-//   }
-
-// }
-
-// // Current Weather from OpenWeather
-
-// function results(data){
-//   "<p>UVIndex: " + data.value + "</p>"
-
-// }
-
+  
 
 
 
